401eda897b70fd04d94f1622644e31448fad2d13
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2022-01-30 15:49:46 +0900

    fix circleci

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule Demo.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
   end
 
